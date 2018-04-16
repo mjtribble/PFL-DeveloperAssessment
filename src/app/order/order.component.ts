74d988b6d27e58105425ea6eb943a7281d94453d
Melody <mjtribble2@gmail.com>
2018-04-15 19:36:00 -0600

    order payload formated

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -18,6 +18,84 @@ export class OrderComponent{
   customer: OrderCustomerData;
   payload: OrderPayload;
 
+
+//   {
+//     "partnerOrderReference": "MyReferenceNumber",
+//     "orderCustomer": {
+//         "firstName": "John",
+//         "lastName": "Doe",
+//         "companyName": "ACME",
+//         "address1": "1 Acme Way",
+//         "address2": "",
+//         "city": "Livingston",
+//         "state": "MT",
+//         "postalCode": "59047",
+//         "countryCode": "US",
+//         "email": "jdoe@acme.com",
+//         "phone": "1234567890"
+//     },
+//     "items": [
+//         {
+//             "itemSequenceNumber": 1,
+//             "productID": 1234,
+//             "quantity": 1000,
+//             "productionDays": 4,
+//             "partnerItemReference": "MyItemReferenceID",
+//             "itemFile": "http://www.yourdomain.com/files/printReadyArtwork1.pdf"
+//         }
+//     ],
+//     "shipments": [
+//         {
+//             "shipmentSequenceNumber": 1,
+//             "firstName": "John",
+//             "lastName": "Doe",
+//             "companyName": "ACME",
+//             "address1": "1 Acme Way",
+//             "address2": "",
+//             "city": "Livingston",
+//             "state": "MT",
+//             "postalCode": "59047",
+//             "countryCode": "US",
+//             "phone": "1234567890",
+//             "shippingMethod": "FDXG",
+//             "IMBSerialNumber":"004543450"
+//         }
+//     ],
+//     "payments":[
+//         {
+//             "paymentMethod": "methodname",
+//             "paymentID": "methodid",
+//             "paymentAmount": 3.00
+//         }
+//     ],
+//     "itemShipments":[
+//         {
+//             "itemSequenceNumber": 1,
+//             "shipmentSequenceNumber":1
+//         }
+//     ],
+//     "webhooks":[
+//         {
+//             "type":"status",
+//             "callbackUri": "https://www.pfl.com/callback",
+//             "callbackHeaders": {
+//                 "header_field_sample1": "header_value_sample1",
+//                 "header_field_sample2": "header_value_sample2"
+//             }
+//         }
+//     ],
+//     "billingVariables":[
+//         {
+//             "key":   "BillingVariable1Name",
+//             "value": "BillingVariable1Value"
+//         },
+//         {
+//             "key":   "BillingVariable2Name",
+//             "value": "BillingVariable2Value"
+//         }
+//     ]
+// }
+
   constructor(
     private route: ActivatedRoute,
     private orderService : OrderService,
@@ -26,6 +104,15 @@ export class OrderComponent{
 
 
   submit():void{
+
+    this.payload =
+    {
+        orderNumber: 87,
+        partnerOrderReference: 'MyReferenceNumber',
+        orderCustomer: this.customer,
+        items: this.orderItems
+    };
+
     this.orderService.postOrder(this.payload)
       .subscribe(ord => {
         this.orders.push(ord);
@@ -38,10 +125,12 @@ export class OrderComponent{
 
   saveCustomer($event) {
     this.customer = $event;
+    this.payload.orderCustomer = this.customer;
   }
 
   saveItem($event) {
     this.orderItems = [$event];
+    this.payload.items = this.orderItems;
   }
 
   get diagnostic1() { return JSON.stringify(this.customer) }
